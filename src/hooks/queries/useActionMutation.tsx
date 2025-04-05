9adafbeacf875dbfcbb121761dcc415bee4033e8
DawidFranczak <daw.franczak@gmail.com>
2025-04-05 14:58:46 +0200

    Added event handling support for the RFID reader

--- src/hooks/queries/useActionMutation.tsx
+++ src/hooks/queries/useActionMutation.tsx
@@ -1,14 +1,16 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import useFetch from "../useFetch";
 import { api } from "../../const/api";
 
 export default function useActionMutation() {
   const { createData } = useFetch();
-
   function createAction(id: number) {
     return useMutation({
       mutationFn: (data: any) =>
         createData(`${api.actionAndEvent}${id}/`, data),
+      onSuccess: (data) => {
+        console.log("Action created successfully", data);
+      },
     });
   }
   return { createAction };
