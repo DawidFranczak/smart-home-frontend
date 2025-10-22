9c5e348aeb108ff2f9a824fca40b92f9d8f716ab
Dawid Franczak <daw.franczak@gmail.com>
2025-10-22 15:25:29 +0200

    Refactor UI of settings selection bar

--- src/components/NavbarSettings/NavbarSettings.tsx
+++ src/components/NavbarSettings/NavbarSettings.tsx
@@ -1,33 +1,26 @@
-import styles from "./NavbarSettings.module.css";
 import { TSettings } from "../../type/TSettings";
-import NavbarSettingsSelect from "../ui/NavbarSettingsSelect/NavbarSettingsSelect";
-interface INavbarSettingsProps {
-  onChange: (value: TSettings) => void;
-  value: string;
-}
-export default function NavbarSettings({
-  onChange,
-  value,
-}: INavbarSettingsProps) {
-  return (
-    <div className={styles.container}>
-      <NavbarSettingsSelect
-        onClick={onChange}
-        value={value}
-        name="passwordChange"
-      >
-        Zmiana hasła
-      </NavbarSettingsSelect>
-      <NavbarSettingsSelect onClick={onChange} value={value} name="homeChange">
-        Zmiana domu
-      </NavbarSettingsSelect>
+import { Nav } from "rsuite";
+import styles from "./NavbarSettings.module.css";
 
-      <NavbarSettingsSelect onClick={onChange} value={value} name="homeCode">
-        Kod domu
-      </NavbarSettingsSelect>
-      <NavbarSettingsSelect onClick={onChange} value={value} name="homeLeave">
-        Wyjdź z domu
-      </NavbarSettingsSelect>
-    </div>
-  );
+interface INavbarSettingsProps {
+    onChange: (value: TSettings) => void;
+    value: TSettings;
+}
+
+export default function NavbarSettings({ onChange, value }: INavbarSettingsProps) {
+    return (
+        <div className={styles.container}>
+            <Nav
+                appearance="subtle"
+                activeKey={value}
+                onSelect={(val) => onChange(val as TSettings)}
+                vertical
+            >
+                <Nav.Item eventKey="passwordChange">Zmiana hasła</Nav.Item>
+                <Nav.Item eventKey="homeChange">Zmiana domu</Nav.Item>
+                <Nav.Item eventKey="homeCode">Kod domu</Nav.Item>
+                <Nav.Item eventKey="homeLeave">Wyjdź z domu</Nav.Item>
+            </Nav>
+        </div>
+    );
 }
