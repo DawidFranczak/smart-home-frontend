bc04b6940d23665f1d1a591f54804e80e7eb6441
Dawid Franczak <daw.franczak@gmail.com>
2026-01-05 12:29:31 +0100

    Update ButtonCard UI

--- src/components/Cards/ButtonCard/ButtonCard.tsx
+++ src/components/Cards/ButtonCard/ButtonCard.tsx
@@ -1,16 +1,18 @@
 import DeviceCardContainer from "../../ui/containers/DeviceCardContainer/DeviceCardContainer";
 import EventButton from "../../ui/Buttons/EventButton/EventButton.tsx";
 import IButton from "../../../interfaces/IButton.tsx";
-import styles from "./ButtonCard.module.css";
+import switchOnOff from "../../../../public/static/svg/switchOnOff.svg"
 export default function ButtonCard(button: IButton) {
   return (
     <DeviceCardContainer
       name={button.name}
       isOnline={button.is_online}
       to={`/button/${button.id}/`}
-    >   <div className={styles.container}>
-            <EventButton id={button.id} buttonType={button.button_type} events={button.events}/>
-        </div>
+      svg={switchOnOff}
+      alt={button.name}
+      >
+        <EventButton id={button.id} buttonType={button.button_type} events={button.events}/>
+        <div></div>
     </DeviceCardContainer>
   );
 }
